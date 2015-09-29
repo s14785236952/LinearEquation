5b6967bd7d904aca42b56d600bb138c31cff5f86
s14785236952 <an_tsai2002@yahoo.com.tw>
2015-09-29 17:45:38 +0800

    增加註解
    增加註解

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,33 +3,40 @@ import java.util.Scanner;
 public class LinearEquation {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		// 要求使用者輸入數值
 		System.out.println("Please Input the Value of a,b,c,d,e and f : ");
+		// 宣告使用者輸入之數值
 		double a = scanner.nextDouble();
 		double b = scanner.nextDouble();
 		double c = scanner.nextDouble();
 		double d = scanner.nextDouble();
 		double e = scanner.nextDouble();
 		double f = scanner.nextDouble();
-		Calculate calculate = new Calculate(a,b,c,d,e,f);
-		System.out.print("When a = "+calculate.getA());
-		System.out.print(" , b = "+calculate.getB());
-		System.out.print(" , c = "+calculate.getC());
-		System.out.print(" , d = "+calculate.getD());
-		System.out.print(" , e = "+calculate.getE());
-		System.out.println(" and f = "+calculate.getF());
-		if(calculate.isSolvable() == true)
-		System.out.println("The Equation's solution :\n"+"x = "+calculate.getX()+" and y = "+calculate.getY());
-		else 
+		// create object
+		Calculate calculate = new Calculate(a, b, c, d, e, f);
+		// 輸出計算完畢後的數值
+		System.out.print("When a = " + calculate.getA());
+		System.out.print(" , b = " + calculate.getB());
+		System.out.print(" , c = " + calculate.getC());
+		System.out.print(" , d = " + calculate.getD());
+		System.out.print(" , e = " + calculate.getE());
+		System.out.println(" and f = " + calculate.getF());
+		if (calculate.isSolvable() == true)
+			System.out.println(
+					"The Equation's solution :\n" + "x = " + calculate.getX() + " and y = " + calculate.getY());
+		// 分母為零所以沒有答案
+		else
 			System.out.println("The Eqution Has No Solution.");
-			
+
 	}
-	
+
 }
 
 class Calculate {
 	private double a, b, c, d, e, f;
 	private double x, y;
 
+	// constructor
 	Calculate(double A, double B, double C, double D, double E, double F) {
 		a = A;
 		b = B;
